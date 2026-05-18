ea76007c69bc4375e9d017e528eb4810c2e52e63
sechang <binaryblitz1412@gmail.com>
2026-05-18 11:00:09 +0900

    fix: 03_main.py 수정

--- lessons/03_main.py
+++ lessons/03_main.py
@@ -15,3 +15,20 @@ async def read_items():
 async def get_information():
     return "이것은 FastAPI 강의 예제 API의 정보입니다."
 
+
+
+@app.post("/items")
+async def create_item():
+    return {"message": "새로운 아이템이 성공적으로 생성되었습니다."}
+
+
+
+@app.put("/items/update-status")
+async def update_item_status():
+    return {"status": "아이템 상태가 업데이트되었습니다."}
+
+
+
+@app.delete("/items/clear-all")
+async def delete_all_items():
+    return {"message": "모든 아이템이 삭제되었습니다."}
