1f2599b6af9e52f099216a70c8b607305fec00ab
sechang <binaryblitz1412@gmail.com>
2026-05-26 13:04:16 +0900

    수정

--- main.py
+++ main.py
@@ -9,3 +9,9 @@ app = FastAPI()
 @app.get("/")
 async def read_root():  # 비동기 함수로 정의합니다.
     return { "message": "Hello World"} # 딕셔너리 형태의 데이터를 반환합니다. FastAPI가 자동으로 JSON 응답으로 변환해줍니다.
+
+"""
+/ 경로로 GET 요청이 들어왔을 때 실행될 API 함수를 정의하는 부분입니다.
+사용자가 http://127.0.0.1:8000/ 같은 기본 주소로 접속하면 read_root() 함수가 실행되고, 
+{"message": "Hello World"}라는 딕셔너리를 반환합니다. FastAPI는 이 딕셔너리를 자동으로 JSON 응답으로 변환해줍니다.
+"""
